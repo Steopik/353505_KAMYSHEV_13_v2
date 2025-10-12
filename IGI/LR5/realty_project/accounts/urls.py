05b4d8bea60b7c87e902728c959137a2a92da7f3
Steopik <kamushev.05@gmail.com>
2025-10-12 14:07:34 +0300

    add css

--- IGI/LR5/realty_project/accounts/urls.py
+++ IGI/LR5/realty_project/accounts/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('confirm-email/<int:user_id>/', views.confirm_email, name='confirm_email'),
     path('login/', views.user_login, name='login'),
